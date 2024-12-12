1d6f4d82c3c695c5649cddd43a267708726c901a
unknown <gregorydealmeida901@gmail.com>
2024-12-12 10:36:12 -0300

    add new styles

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import LOGO from './img/blocks-logo.png'
-import style from './menu.module.css'
+import styles from './menu.module.css'
 
 export default function Menu() {
   return (
-    <>
-      <main className={style.main}>
-        <Link href='/'>
-          <Image className={style.img} src={LOGO} alt="Logo Blocks"/>
-        </Link>
-      </main>
-      <hr className={style.hr} />
-    </>
+    <main className={styles.main}>
+      <Link href='/'>
+        <Image className={styles.img} src={LOGO} alt="Logo Blocks"/>
+      </Link>
+    </main>
   )
 }
