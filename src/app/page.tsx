6e78ae48912297e0e0cdc0e8e6f15a11f368c30e
unknown <gregorydealmeida901@gmail.com>
2024-12-15 00:55:58 -0300

    fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,8 +73,8 @@ export default function HomePage() {
                     </span>
                     <span className={styles.arrow}>
                       <svg
-                        stroke="currentColor"
-                        fill="currentColor"
+                        stroke="black"
+                        fill="black"
                         strokeWidth="0"
                         viewBox="0 0 24 24"
                         height="1rem"
